071d44e1a1302338930e84a2d6ac168a3ed01e7d
Daniel Mekonnen <daneymail@gmail.com>
2017-11-28 16:14:01 +0100

    I added arrow controlling option

--- ASP_RobotWebcamJS_Ny-master/WebCamJS/Controllers/WebcamController.cs
+++ ASP_RobotWebcamJS_Ny-master/WebCamJS/Controllers/WebcamController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,16 +11,33 @@ namespace WebCamJS.Controllers
 {
     public class WebcamController : Controller
     {
+
+        public byte[] sendbuf;
         // GET: Webcam
         public ActionResult Index()
         {
             return View();
         }
 
-        
+
         public ActionResult ReloadWebcam()
         {
             return View("Index");
+        }
+
+        public ActionResult TestingAction(int? id)
+        {
+            ViewBag.Key = id.ToString();
+            sendbuf = Encoding.ASCII.GetBytes(id.ToString());
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+              ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("192.168.4.1");
+            IPEndPoint ep = new IPEndPoint(broadcast, 1025);
+            s.SendTo(sendbuf, ep);
+
+            return View();
         }
     }
 }
---
+++ ASP_RobotWebcamJS_Ny-master/WebCamJS/Scripts/robotArrowNavigation.js
@@ -0,0 +1,33 @@
+﻿document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            location.href = '/Webcam/TestingAction?id=' + 3;//left
+            break;
+        case 38:
+            location.href = '/Webcam/TestingAction?id=' + 1;//up
+            break;
+        case 39:
+            location.href = '/Webcam/TestingAction?id=' + 4;//right
+            break;
+        case 40:
+            location.href = '/Webcam/TestingAction?id=' + 2;//down
+            break;
+    }
+};
+
+document.onkeyup = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            location.href = '/Webcam/TestingAction?id=' + 5;//left
+            break;
+        case 38:
+            location.href = '/Webcam/TestingAction?id=' + 5;//up
+            break;
+        case 39:
+            location.href = '/Webcam/TestingAction?id=' + 5;//right
+            break;
+        case 40:
+            location.href = '/Webcam/TestingAction?id=' + 5;//down
+            break;
+    }
+};
